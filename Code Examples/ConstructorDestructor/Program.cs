bda77ef9614461ad2bbe894f2091450943caae99
Cotfas Liviu-Adrian <lcotfas@gmail.com>
2016-03-28 17:49:50 +0300

    Changed inheritance example

--- Code Examples/ConstructorDestructor/Program.cs
+++ Code Examples/ConstructorDestructor/Program.cs
@@ -5,37 +5,90 @@ namespace ConstructorDestructor
 {
 	internal class Program
 	{
-		private static void Main(string[] args)
+		private static void Main()
 		{
-			AbstractNormalVirtualMethods();
+			//AbstractNormalVirtualMethods();
+			//AbstractNormalVirtualMethodsArray();
+			CustomInterface();
 		}
 
 		private static void AbstractNormalVirtualMethods()
 		{
-			var softwareDeveloper = new SoftwareDeveloper(2000);
-
-			//Abstract method
-			Console.WriteLine("\n###Abstract");
-			Console.WriteLine(softwareDeveloper.CalculateBonusAbstract());
-			Console.WriteLine(((Employee)softwareDeveloper).CalculateBonusAbstract());
+			SoftwareDeveloper softwareDeveloper = new SoftwareDeveloper("SoftwareDeveloper1");
+			Employee employeeReference = (Employee) softwareDeveloper; //same instance as above
 
 			//Normal method
 			Console.Write("\n###Hide");
-			Console.WriteLine(softwareDeveloper.CalculateBonusNormal());
-			Console.WriteLine(((Employee)softwareDeveloper).CalculateBonusNormal());
+			softwareDeveloper.PrintWageNormal();
+			employeeReference.PrintWageNormal();
 
 			//Virtual method
 			Console.Write("\n###Override");
-			Console.WriteLine(softwareDeveloper.CalculateBonusVirtual());
-			Console.WriteLine(((Employee)softwareDeveloper).CalculateBonusVirtual());
+			softwareDeveloper.PrintWageVirtual();
+			employeeReference.PrintWageVirtual();
+
+			//Abstract method
+			Console.WriteLine("\n###Abstract");
+			softwareDeveloper.PrintWageAbstract();
+			employeeReference.PrintWageAbstract();
+
 		}
 
-		private static void UpCastDownCast()
+		private static void AbstractNormalVirtualMethodsArray()
 		{
-			//var employee = new Employee(2000);
-			//Person person = employee;
+			var softwareDeveloper = new SoftwareDeveloper("SoftwareDeveloper1");
+			var manager = new Manager("Manager1");
 
-			//Employee employee2 = (Employee) person;
+			Employee[] employees = new Employee[] {softwareDeveloper, manager};
+
+			foreach (var employee in employees)
+				employee.PrintWageNormal();
+
+			foreach (var employee in employees)
+				employee.PrintWageVirtual();
+
+			foreach (var employee in employees)
+				employee.PrintWageAbstract();
+		}
+
+		private static void CustomInterface()
+		{
+			//var softwareDeveloper = new SoftwareDeveloper {KnownProgrammingLanguages = new[] {"C#", "Java"}};
+			var softwareDeveloper = new SoftwareDeveloper("SoftwareDeveloper1");
+			softwareDeveloper.KnownProgrammingLanguages = new[] {"C#", "Java"};
+
+			var manager = new Manager("Manager1");
+
+			var contractor = new Contractor("Contractor1");
+			contractor.KnownProgrammingLanguages = new[] {"C#"};
+
+			Person[] persons = new Person[] {softwareDeveloper, manager, contractor};
+
+			Console.WriteLine("###Using \"is\"");
+			foreach (var person in persons)
+			{
+				if (person is IKnownProgrammingLanguages)
+				{
+					Console.WriteLine("{0}: {1}",
+						person.Name,
+						string.Join(", ", ((IKnownProgrammingLanguages) person).KnownProgrammingLanguages));
+				}
+			}
+			Console.WriteLine();
+
+			Console.WriteLine("###Using \"as\"");
+			foreach (var person in persons)
+			{
+				//IKnownProgrammingLanguages castToInterface = person as IKnownProgrammingLanguages;
+				var castToInterface = person as IKnownProgrammingLanguages;
+				
+				if (castToInterface != null)
+				{
+					Console.WriteLine("{0}: {1}",
+						person.Name, 
+						string.Join(", ", castToInterface.KnownProgrammingLanguages));
+				}
+			}
 		}
 	}
 }
