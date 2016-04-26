9d87122242c0f6bf19265d342493b409296984fd
Cotfas Liviu-Adrian <lcotfas@gmail.com>
2016-04-26 18:40:08 +0300

    Updated PieChartGraphicsSample

--- Code Examples/PieChartGraphicsSample/PieChartCOntrol.Designer.cs
+++ Code Examples/PieChartGraphicsSample/PieChartCOntrol.Designer.cs
@@ -1,13 +1,13 @@
 ﻿namespace PieChartGraphicsSample
 {
-	partial class MainForm
+	partial class PieChartControl
 	{
-		/// <summary>
+		/// <summary> 
 		/// Required designer variable.
 		/// </summary>
 		private System.ComponentModel.IContainer components = null;
 
-		/// <summary>
+		/// <summary> 
 		/// Clean up any resources being used.
 		/// </summary>
 		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
@@ -20,40 +20,28 @@
 			base.Dispose(disposing);
 		}
 
-		#region Windows Form Designer generated code
+		#region Component Designer generated code
 
-		/// <summary>
-		/// Required method for Designer support - do not modify
+		/// <summary> 
+		/// Required method for Designer support - do not modify 
 		/// the contents of this method with the code editor.
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.pieChartControl1 = new PieChartGraphicsSample.PieChartControl();
 			this.SuspendLayout();
 			// 
-			// pieChartControl1
-			// 
-			this.pieChartControl1.Location = new System.Drawing.Point(12, 12);
-			this.pieChartControl1.Name = "pieChartControl1";
-			this.pieChartControl1.Size = new System.Drawing.Size(418, 323);
-			this.pieChartControl1.TabIndex = 0;
-			// 
-			// MainForm
+			// PieChartControl
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(456, 351);
-			this.Controls.Add(this.pieChartControl1);
-			this.Name = "MainForm";
-			this.Text = "PieChart Sample";
-			this.Load += new System.EventHandler(this.MainForm_Load);
+			this.BackColor = System.Drawing.Color.White;
+			this.Name = "PieChartControl";
+			this.Size = new System.Drawing.Size(458, 363);
+			this.Paint += new System.Windows.Forms.PaintEventHandler(this.PieChartControl_Paint);
 			this.ResumeLayout(false);
 
 		}
 
 		#endregion
-
-		private PieChartControl pieChartControl1;
 	}
 }
-
---
+++ Code Examples/PieChartGraphicsSample/PieChartCOntrol.cs
@@ -0,0 +1,89 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+using PieChartGraphicsSample.Entities;
+
+namespace PieChartGraphicsSample
+{
+	public partial class PieChartControl : UserControl
+	{
+		#region Properties
+		private PieChartCategory[] _data;
+		public PieChartCategory[] Data {
+			get { return _data; }
+			set
+			{
+				if(_data == value)
+				return;
+				_data = value;
+				Invalidate();
+			}
+		}
+		#endregion
+
+		public PieChartControl()
+		{
+			InitializeComponent();
+			ResizeRedraw = true;
+
+			//Default data
+			Data = new[]
+			{
+				new PieChartCategory("Category 1", 20, Color.Red),
+				new PieChartCategory("Category 2", 80, Color.Blue),
+			};
+		}
+
+		private void PieChartControl_Paint(object sender, PaintEventArgs e)
+		{
+			Graphics graphics = e.Graphics;
+
+			float percent1 = 0;
+			float percent2 = 0;
+			const float radius = 75;
+			const int xCenter = 90;
+			const int yCenter = 150;
+
+			const float x = xCenter - radius;
+			const float y = yCenter - radius;
+			const float width = radius * 2;
+			const float height = radius * 2;
+
+			for (int i = 0; i < Data.Length; i++)
+			{
+				if (i >= 1)
+					percent1 += Data[i - 1].Percentage;
+
+				percent2 += Data[i].Percentage;
+
+				float angle1 = percent1 / 100 * 360;
+				float angle2 = percent2 / 100 * 360;
+
+				Brush b = new SolidBrush(Data[i].Color);
+
+				graphics.FillPie(b, x, y, width, height, angle1, angle2 - angle1);
+			}
+
+			Pen pen = new Pen(Color.Black);
+
+			graphics.DrawEllipse(pen, x, y, width, height);
+
+			const float xpos = x + width + 20;
+			float ypos = y - 25;
+
+			for (int i = 0; i < Data.Length; i++)
+			{
+				Brush b = new SolidBrush(Data[i].Color);
+
+				graphics.FillRectangle(b, xpos, ypos, 30, 30);
+				graphics.DrawRectangle(pen, xpos, ypos, 30, 30);
+
+				Brush b2 = new SolidBrush(Color.Black);
+
+				graphics.DrawString(Data[i].Description + ": " + Data[i].Percentage + "%", Font, b2,
+					xpos + 35, ypos + 12);
+
+				ypos += 35;
+			}
+		}
+	}
+}
